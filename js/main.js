711e121b12c2c49d69136c15ca5f7004f85076da
unknown <triptoroy5@gmail.com>
2022-05-15 03:10:19 +0600

    about section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+let element = document.getElementsByClassName("element")
+
+var typed = new Typed(element, {
+  strings: ["ge","asdsadf"],
+  typeSpeed: 140,
+  backspeed: 140,
+  loop: true
+});
