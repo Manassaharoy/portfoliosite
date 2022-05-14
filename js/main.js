8f5a033ca95e96866aac44f03f639a26cbc5adcb
unknown <triptoroy5@gmail.com>
2022-05-15 03:12:44 +0600

    updated js file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import Typed from 'typed.js';
-let element = document.getElementsByClassName("element")
-
-var typed = new Typed(element, {
-  strings: ["ge","asdsadf"],
-  typeSpeed: 140,
-  backspeed: 140,
-  loop: true
-});
