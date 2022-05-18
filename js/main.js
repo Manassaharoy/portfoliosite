4ff4f89e3ffb616fd9ed3ac20d1545efc68f7262
unknown <triptoroy5@gmail.com>
2022-05-19 00:15:47 +0600

    responsive update

--- js/main.js
+++ js/main.js
@@ -73,9 +73,9 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener("change", switchTheme, false);
 
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
-})
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".navbar-fixed-top");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// })
